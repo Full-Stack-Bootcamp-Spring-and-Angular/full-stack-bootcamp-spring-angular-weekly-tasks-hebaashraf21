a1f2eb20ef73747d854225415ee7e39df237084f
hebaashraf21 <heba.elmaaboud00@eng-st.cu.edu.eg>
2025-12-26 20:23:18 +0200

    Finish Task 3.1: Bean Scope Types

---
+++ Task3.1_bean_scope_types/src/Draw2D.java
@@ -0,0 +1,7 @@
+public class Draw2D {
+
+    void draw(String shape)
+    {
+        System.out.println(String.format("Drawing a 2D for: %s",shape));
+    }
+}
---
+++ Task3.1_bean_scope_types/src/Draw3D.java
@@ -0,0 +1,6 @@
+public class Draw3D {
+    void draw(String shape)
+    {
+        System.out.println(String.format("Drawing a 3D for: %s",shape));
+    }
+}
