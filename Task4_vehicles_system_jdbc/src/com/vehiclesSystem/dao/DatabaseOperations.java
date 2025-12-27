25b148bc417d5ae760fccaf36f42e55513feae92
hebaashraf21 <heba.elmaaboud00@eng-st.cu.edu.eg>
2025-12-27 13:09:32 +0200

    add: database connection established

--- Task4_vehicles_system_jdbc/src/com/vehiclesSystem/dao/DatabaseOperations.java
+++ Task4_vehicles_system_jdbc/src/com/vehiclesSystem/dao/DatabaseOperations.java
@@ -3,7 +3,6 @@ package com.vehiclesSystem.dao;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -37,7 +36,7 @@ public class DatabaseOperations {
         System.out.println(String.format("Connecting to database [%s, %s, %s]...", url,username,password));
         try {
             connection = DriverManager.getConnection(url, username, password);
-            System.out.println(" Database connection established");
+            System.out.println("Database connection established");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -49,7 +48,7 @@ public class DatabaseOperations {
         System.out.println(String.format("Connecting to database [%s, %s, %s]...", url,username,password));
         try {
             connection.close();
-            System.out.println(" Database connection closed");
+            System.out.println("Database connection closed");
         } catch (Exception e) {
             e.printStackTrace();
         }
